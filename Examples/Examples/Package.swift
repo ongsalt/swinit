c2c32b63162ed86631221da0e624cc7fda161e55
Ongsalt <ongsa.srp.mkm@gmail.com>
2026-07-14 03:11:28 +0700

    remove gnome crashing code

--- Examples/Examples/Package.swift
+++ Examples/Examples/Package.swift
@@ -8,33 +8,12 @@ let package = Package(
         .package(url: "https://github.com/ongsalt/SwiftWayland", branch: "master", traits: ["XDG", "WP"]),
     ],
     targets: [
-        .systemLibrary(
-            name: "CairoLib",
-            pkgConfig: "cairo",
-            providers: [.apt(["libcairo2-dev"]), .brew(["cairo"])]
-        ),
         .executableTarget(
             name: "Examples",
             dependencies: [
                 .product(name: "Swinit", package: "swinit"),
                 .product(name: "WaylandClient", package: "SwiftWayland", condition: .when(platforms: [.linux])),
             ]
-        ),
-        .executableTarget(
-            name: "CrashGnome",
-            dependencies: [
-                .product(name: "WaylandClient", package: "SwiftWayland"),
-            ],
-            exclude: ["crash_gnome.c"]
-        ),
-        .executableTarget(
-            name: "CrashGnomeUI",
-            dependencies: [
-                .product(name: "Swinit", package: "swinit"),
-                .product(name: "WaylandClient", package: "SwiftWayland"),
-                "CairoLib",
-            ],
-            path: "Sources/CrashGnomeUI"
         ),
     ],
     swiftLanguageModes: [.v6]
